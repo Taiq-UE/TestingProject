597cd908482d8b39c21719dbd20104fba2f51d22
2007 2007 <E2007@365i.team>
2024-04-16 07:54:21 +0200

    added tests revised

--- TestingServerPython/test_main.py
+++ TestingServerPython/test_main.py
@@ -1,8 +1,62 @@
 import unittest
 from unittest.mock import patch
-from main import get_albums, get_users, get_photos, get_post_comments
+from main import get_albums, get_users, get_photos, get_post_comments, get_posts
 
 class TestMain(unittest.TestCase):
+
+    @patch('main.requests.get')
+    def test_get_posts(self, mock_get):
+        # Ustawiamy mocka dla funkcji requests.get
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = [
+            {
+                "userId": 1,
+                "id": 1,
+                "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+            }
+        ]
+
+        # Wywołujemy funkcję, którą testujemy
+        response = get_posts(1)
+
+        # Sprawdzamy, czy funkcja zwraca oczekiwane dane
+        self.assertEqual(response, mock_get.return_value.json.return_value)
+
+    @patch('main.requests.get')
+    def test_get_posts_status_code_not_200(self, mock_get):
+        # Ustawiamy mocka dla funkcji requests.get
+        mock_get.return_value.status_code = 500
+
+        # Wywołujemy funkcję, którą testujemy
+        response = get_posts(1)
+
+        # Sprawdzamy, czy funkcja zwraca odpowiednią odpowiedź
+        self.assertEqual(response, ({'error': 'An error occurred while fetching posts'}, 500))
+
+    @patch('main.requests.get')
+    def test_get_posts_empty_json(self, mock_get):
+        # Ustawiamy mocka dla funkcji requests.get
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = []
+
+        # Wywołujemy funkcję, którą testujemy
+        response = get_posts(1)
+
+        # Sprawdzamy, czy funkcja zwraca pustą listę
+        self.assertEqual(response, [])
+
+    @patch('main.requests.get')
+    def test_get_posts_invalid_json(self, mock_get):
+        # Ustawiamy mocka dla funkcji requests.get
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {'message': 'Invalid JSON'}
+
+        # Wywołujemy funkcję, którą testujemy
+        response = get_posts(1)
+
+        # Sprawdzamy, czy funkcja zwraca odpowiednią odpowiedź
+        self.assertEqual(response, {'message': 'Invalid JSON'})
 
     @patch('main.requests.get')
     def test_get_albums(self, mock_get):
